f1219bf45369b727a7df235caa5f5bd4ba04db74
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2016-03-27 00:53:53 -0400

    Fizzbuzz prints 'FizzBuzz' when number is multiple of three and five.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,17 @@
 class FizzBuzz
 	def fizzbuzz number
-		if number % 3 == 0 then
-			"Fizz"
-		elsif number % 5 == 0 then
-			"Buzz"
+		if number % 3 == 0 || number % 5 == 0 then
+			result = ""
+			if number % 3 == 0 then
+				result += "Fizz"
+			end
+			if number % 5 == 0 then
+				result += "Buzz"
+			end
+
+			return result
 		else
-			"#{number}"
+				"#{number}"
 		end
 	end
 end
