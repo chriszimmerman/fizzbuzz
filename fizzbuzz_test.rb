acbbb801f3a82daafb41327cbbaf100d30667016
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2016-03-27 00:27:32 -0400

    Writes failing test to print a number that is not a multiple of three or five.

--- fizzbuzz_test.rb
+++ fizzbuzz_test.rb
@@ -5,4 +5,8 @@ class TestFizzbuzz < Minitest::Test
 	def setup
 		@sut = FizzBuzz.new
 	end
+
+	def test_fizzbuzz_number_is_not_multiple_of_three_or_five
+		assert_equal("1", @sut.fizzbuzz(1))
+	end
 end
