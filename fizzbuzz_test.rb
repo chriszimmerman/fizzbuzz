4e618661cb1ee3fddc9684a2e01b8a1811e27c7f
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2016-03-27 00:30:07 -0400

    Adds more test cases for printing a number to drive a change in design.

--- fizzbuzz_test.rb
+++ fizzbuzz_test.rb
@@ -8,5 +8,7 @@ class TestFizzbuzz < Minitest::Test
 
 	def test_fizzbuzz_number_is_not_multiple_of_three_or_five
 		assert_equal("1", @sut.fizzbuzz(1))
+		assert_equal("2", @sut.fizzbuzz(2))
+		assert_equal("4", @sut.fizzbuzz(4))
 	end
 end
