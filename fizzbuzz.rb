244eb3cb0368ba9aceed62a33fc4d5bfa8029000
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2016-03-28 20:53:05 -0400

    Refactoring that makes the test pass.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,17 @@
 class FizzBuzz
 	def fizzbuzz number
+		if number % 3 == 0 || number % 5 == 0 then
+			result = ""
 			if number % 3 == 0 then
-				"Fizz"
-			elsif	number % 5 == 0 then
-				"Buzz"
-			else	
-				"#{number}"
+				result += "Fizz"
 			end
+			if number % 5 == 0 then
+				result += "Buzz"
+			end
+
+			return result
+		else
+				"#{number}"
+		end
 	end
 end
